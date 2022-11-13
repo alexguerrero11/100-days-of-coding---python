3616fd4b65b6e2979a955a0bc791f9e6c814efd7
alexguerrero11 <55722397+alexguerrero11@users.noreply.github.com>
2022-11-13 18:41:44 -0500

    updated files

--- DAY14/higher_lower.py
+++ DAY14/higher_lower.py
@@ -5,50 +5,51 @@ import random #random package needed
 import higher_lower_art #clip art
 from higher_lower_data import data #game data
 
-length = len(data) #determine how much data we got
-
-
-def get_random_person():
-    #function that generates a random int and returns data using it as an index
-    random_int = random.randint(1,length)
-    return(data[random_int])
-
 def compare_followers(personA, personB):
     #function compares followers and returns letter
     if personA['follower_count'] > personB['follower_count']:
         return "A"
     else:
-        return "B"    
-    
-# Game
+        return "B"
+
 def game():
+    
     print(higher_lower_art.logo) #1)print logo
     
-    #2)select Person A
-    personA = get_random_person()
-    message1 = f"Compare A: {personA['name']}, {personA['description']} from {personA['country']}"
-    print(message1)
+    #initialize variables
+    personA = random.choice(data)
+    personB = random.choice(data)
     
-    print(higher_lower_art.vs) #3)print VS
+    #pre-req
+    game_continues = True
+    score = 0  #initialize score
     
-    #4)select Person B
-    personB = get_random_person()
-    message2 = f"Compare B: {personB['name']}, {personB['description']} from {personB['country']}"
-    print(message2)
-    
-    #asking for user's input
-    user_input = input('Q: Who has more followers? Type "A" or "B": ')
-    
-    #actual winner
-    winner = compare_followers(personA,personB)
-    
-    #score
-    score = 0
-    
-    #user's response feedback
-    if user_input != winner:
-        print('Sorry, that is wrong. Final Score: ')
-    elif user_input == winner:
-        print(f'You are right! Current Score: ')
+    #game starts
+    while game_continues == True:        
+        personA = personB
+        personB = random.choice(data)
+        
+        # Main Message Displayed
+        message1 = f"Compare A: {personA['name']}, {personA['description']} from {personA['country']}"
+        print(message1)
+        print(higher_lower_art.vs)
+        message2 = f"Compare B: {personB['name']}, {personB['description']} from {personB['country']}"
+        print(message2)
+        
+        # DEBUGGING - ANSWER
+        print(compare_followers(personA,personB))
+        
+        # User's Input
+        user_input = input('Q: Who has more followers? Type "A" or "B": ')
+        # Actaul winner
+        winner = compare_followers(personA,personB)
+                
+        # Checking if game can continue
+        if user_input == winner:
+            score = score + 1
+            print(f'You are right! Current Score: {score}')
+        else:
+            game_continues = False
+            print(f'Sorry, that is wrong. Final Score: {score}')
 
 game()
--- README.md
+++ README.md
@@ -26,10 +26,10 @@
 +  [Day 13](https://github.com/alexguerrero11/100-days-of-coding-python/tree/main/DAY13) - Debugging: How to Find and Fix Errors in your Code
 +  [Day 14](https://github.com/alexguerrero11/100-days-of-coding-python/tree/main/DAY14) - Higher Lower Game Project
     + Project: Higher Lower Game
++  Day 15 - Local Development Environment Setup & the Coffee Machine
+    + Project: Coffee Machine
 
 Upcoming
-+  Day 15 - Local Development Environment Setup & the Coffee Machine
-    + Coffee Machine
 +  Day 16 - Object Oriented Programming (OOP)
 +  Day 17 - The Quiz Project & The Benefits of OOP
 +  Day 18 - Turtle & the Graphical User Interface (GUI)
